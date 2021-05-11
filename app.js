5f0156c13e59d2714cd72149b9bc38a8f7b8a54e
Felipe Borges <fcborges_@hotmail.com>
2021-05-10 21:37:52 -0300

    CRUD done

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const PORT = 4000;
+
+const app = express();
+
+// Importando as configurações do banco de dados e inicializando a conexão
+const db = require("./config/db.config");
+db();
+
+// Configurar o express para entender requisições contendo JSON no corpo
+app.use(express.json());
+
+// Importa e configura nossas rotas
+const roomsRouter = require("./routes/room.routes");
+app.use("/", roomsRouter);
+
+
+// Inicia o servidor para escutar requisições HTTP na porta 4000
+app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
